894428986635cdd21249d9823a199bf839799887
Tom Burns <tom@burns.ca>
2019-11-28 09:56:00 -0500

    import reusable UI

---
+++ BurnsAudio/UI/ParameterView.swift
@@ -0,0 +1,13 @@
+//
+//  ParameterView.swift
+//  iOSInstrumentDemoFramework
+//
+//  Created by tom on 2019-05-27.
+//
+
+import Foundation
+import UIKit
+
+protocol ParameterView: class {
+    var value: Float { get set }
+}
