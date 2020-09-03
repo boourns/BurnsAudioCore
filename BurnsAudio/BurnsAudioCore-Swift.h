559a0801427831827bb226bdd7ac49568900a391
Tom Burns <tom@burns.ca>
2020-09-03 14:58:33 -0400

    rename to BurnsAudioCore

---
+++ BurnsAudio/BurnsAudioCore-Swift.h
@@ -0,0 +1,13 @@
+//
+//  BurnsAudioCore-Swift.h
+//  BurnsAudioCore
+//
+//  Created by tom on 2020-09-03.
+//  Copyright © 2020 tom. All rights reserved.
+//
+
+#ifndef BurnsAudioCore_Swift_h
+#define BurnsAudioCore_Swift_h
+
+
+#endif /* BurnsAudioCore_Swift_h */
